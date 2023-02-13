37ce84240e490bdf67ae8fd74a1800a7f4dd228e
JamesMcN01 <jamesMN97@gmail.com>
2023-02-13 18:22:11 +0000

    Added Score Counter, Health Bar , Fuel Bar
    By James

---
+++ Yaris Driver/Assets/HealthManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Image healthbar;
+    public float healthAmount = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (healthAmount <=0)
+        {
+             Application.LoadLevel(Application.loadLevel);
+        } */
+
+      
+       
+    }
+
+    public void TakeDamage(float damage)
+    {
+        healthAmount -=damage;
+        healthbar.fillAmount = healthAmount /100f;
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount +=healingAmount;
+
+        healthAmount = Mathf.Clamp(healthAmount, 0 , 100);
+
+        healthbar.fillAmount = healthAmount / 100f;
+    }
+
+
+    void onClick()
+    {
+        
+        
+            TakeDamage(20);
+        
+    }
+
+    void onFire()
+    {
+        
+            Heal(5);
+        
+    }
+}
---
+++ Yaris Driver/Assets/scoreCounter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scoreCounter : MonoBehaviour
+{
+      // used to acces the text object
+    public Text scoreText;
+    // used to store the score total
+    public float scoreTotal;
+    // used to declare the amount of score that is increased per seconed
+    public float scoreIncreasePerSecond;
+    // Start is called before the first frame update
+    void Start()
+    {
+         // sets the total score to 0
+        scoreTotal = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // converts the text to the current score
+        scoreText.text = "Score: " + (int)scoreTotal;
+        // used to increase the score by the set amount per second
+        scoreTotal += scoreIncreasePerSecond * Time.deltaTime;
+    }
+}
