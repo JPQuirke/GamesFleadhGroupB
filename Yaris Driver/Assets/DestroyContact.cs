2e7eadeca5bac4b35fd21334cb54b6998380a594
Mike14533 <123557009+Mike14533@users.noreply.github.com>
2023-03-02 16:26:26 +0000

    Object Spawner halfway done
    Submitted by Michael

---
+++ Yaris Driver/Assets/DestroyContact.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DestroyContact : MonoBehaviour
+{
+    
+    
+    //Get a reference to the Text UI
+    
+
+    
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag =="Obstacle")
+        {
+            GameObject collectable = collision.gameObject;
+
+            Destroy(collectable);
+
+        
+            
+        }
+        
+        
+    }
+}
--- Yaris Driver/Assets/SpawnObstacles.cs
+++ Yaris Driver/Assets/SpawnObstacles.cs
@@ -1,15 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnObstacles : MonoBehaviour
 {
-    public GameObject obstacle;
-    public float maxX;
+
+   [Serializable]
+   public struct SpawnData
+   {
+       public GameObject obstacle_top;
+       public GameObject obstacle_bottom;
+       
+   }
+
+
+    public GameObject[] test;
+    
+    public  SpawnData[] pattern;
+
+   
+
+    private int patternIndex = 0;
+
+    //public float speed =0;
+    
+    /*public float maxX;
     public float minX;
     public float maxY;
-    public float minY;
-    public float timeBetweenSpawn;
+    public float minY;*/
+
     private float spawnTime;
     // Start is called before the first frame update
    
@@ -21,24 +41,29 @@ public class SpawnObstacles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         float randomNumber = Random.Range(0, 9);
-        if(Time.time/2 == 0){
+         float timeBetweenSpawn = 5;
+       
 
                 if(Time.time > spawnTime)
             {
                 Spawn();
-                spawnTime= randomNumber;
+            
+                spawnTime= Time.time + timeBetweenSpawn;
+                Debug.Log("Item spawned");
+                
             }
 
-        }
+        
         
     }
 
     void Spawn()
     {
-        float randomX = Random.Range(minX, maxX);
-        float randomY = Random.Range(minY, maxY);
+        SpawnData data = pattern[patternIndex];
+        //float randomX = Random.Range(minX, maxX);
+        //float randomY = Random.Range(minY, maxY);
 
-        Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
+        Instantiate(data.obstacle_top, transform.position + new Vector3(29, 2, 0), transform.rotation);
+        Instantiate(data.obstacle_bottom, transform.position + new Vector3(29, -2, 0), transform.rotation);
     }
 }
