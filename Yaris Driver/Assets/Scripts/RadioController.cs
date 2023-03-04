c757e19506f62d0458bcd62a6dc861333ed72188
ConorGeraghty27 <123557232+ConorGeraghty27@users.noreply.github.com>
2023-03-04 13:23:33 +0000

    Code Commented
    Comments added to code

--- Yaris Driver/Assets/Scripts/RadioController.cs
+++ Yaris Driver/Assets/Scripts/RadioController.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class RadioController : MonoBehaviour
 {
-    public AudioSource audioSource;
-    public AudioClip[] audioClips;
-    private int currentClipIndex = 0;
+    public AudioSource audioSource; // The component used to play audio
+    public AudioClip[] audioClips; // An array of audio clips to play from
+    private int currentClipIndex = 0; // The index of the currently playing song
 
     void Start()
     {
-        audioSource.clip = audioClips[currentClipIndex];
+        audioSource.clip = audioClips[currentClipIndex]; // Set the clip to the first audio clip in the array
+        audioSource.Play(); // Start playing the radio
     }
 
     void Update()
@@ -16,34 +17,34 @@ public class RadioController : MonoBehaviour
      
     }
 
-       void OnSpace()
+    void OnSpace() // Function to handle the space key press
+    {
+        if (audioSource.isPlaying) // If the audio is playing
         {
-            if (audioSource.isPlaying)
-            {
-                audioSource.Pause();
-            }
-            else
-            {
-                audioSource.Play();
-            }
+            audioSource.Pause(); // Pause the audio
         }
+        else
+        {
+            audioSource.Play(); // Otherwise, play the audio
+        }
+    }
 
-        void OnF()
-        {
-            currentClipIndex = (currentClipIndex + 1) % audioClips.Length;
-            audioSource.clip = audioClips[currentClipIndex];
-            audioSource.Play();
-            Debug.Log("Sound is playing");
-        }
+    void On2() // Function to handle if the 2 key is pressed
+    {
+        currentClipIndex = (currentClipIndex + 1) % audioClips.Length; // Increment by 1 the index and wrap around if necessary
+        audioSource.clip = audioClips[currentClipIndex]; // Set the clip to the new audio clip
+        audioSource.Play(); // Play the new audio clip
+        Debug.Log("Sound is playing"); // Give a printed confirmation that sound is playing
+    }
 
-        void OnG()
+    void On1() // Function to handle if the 1 key is press
+    {
+        currentClipIndex--; // Decrement the index by -1
+        if (currentClipIndex < 0) // If the index is less than zero
         {
-            currentClipIndex--;
-            if (currentClipIndex < 0)
-            {
-                currentClipIndex = audioClips.Length - 1;
-            }
-            audioSource.clip = audioClips[currentClipIndex];
-            audioSource.Play();
+            currentClipIndex = audioClips.Length - 1; // Wrap around to the end of the array
         }
+        audioSource.clip = audioClips[currentClipIndex]; // Set the clip to the new audio clip
+        audioSource.Play(); // Play the new audio clip
+    }
 }
