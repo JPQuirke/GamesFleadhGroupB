09bc1a5eb497411b821acdd3ef7dab513057e666
Piotr Warkocki <bobstewart924@gmail.com>
2023-03-06 21:53:52 +0000

    Revert "Revert "HILK EFFECT partially working(timer needs to  be updated)""
    This reverts commit 022fcc52f46a2c15af7db1ce3b75bb0e3ef99330.

--- Yaris Driver/Assets/Scripts/HIlkBehaviour.cs
+++ Yaris Driver/Assets/Scripts/HIlkBehaviour.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class HIlkBehaviour : MonoBehaviour
+
 {
     //Reference for the Hilkaudio in PlayerInventory
     public AudioClip  audioFile;
-    
+    public GameObject mainCamera;
+    public float timeDelay = 5;
+    public bool DrunkJoshie=false;
+   public  PostProcessVolume ppv;
+   float timeDecrease=1;
+
+    public void Start()
+    {
+        PostProcessVolume ppv = mainCamera.GetComponent<PostProcessVolume>();
+    }
+    void Update()
+    {
+        if (DrunkJoshie==true)
+        {
+            timeDelay-=timeDecrease * Time.deltaTime;
+
+        }
+        if (timeDelay<0)
+        {
+            Drunk();
+        }
+    }
     
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -19,6 +42,15 @@ public class HIlkBehaviour : MonoBehaviour
             
             //gameObject (TheHilk) is destroyed on collision
             Destroy(gameObject);
+
+            
+            ppv.enabled = true;
+
+            Drink();
+
+            
+
+            
         }
 
         //If the hilk collides with the obstacleMagnet then it also gets destroyed
@@ -26,5 +58,19 @@ public class HIlkBehaviour : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    
+    void Drunk()
+    {
+        DrunkJoshie=false;
+        ppv.enabled = false;
+         
+        
+        
+    }
+
+    void Drink()
+    {
+        DrunkJoshie=true;
     }
 }
