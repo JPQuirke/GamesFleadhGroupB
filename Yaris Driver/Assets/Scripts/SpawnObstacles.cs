e87f8ad098aea580878eceaaf7e56d3d7ceb5246
Piotr Warkocki <bobstewart924@gmail.com>
2023-03-07 16:25:24 +0000

    Comments done
    -Micheal and piotr

--- Yaris Driver/Assets/Scripts/SpawnObstacles.cs
+++ Yaris Driver/Assets/Scripts/SpawnObstacles.cs
@@ -10,15 +10,18 @@ public class SpawnObstacles : MonoBehaviour
 
 
 
-
+  //creating two arrays, one to represent the objects being spawned on the top bit of the road and the other for the bottom bit
     public GameObject[] obstacle_top;
     public GameObject[] obstacle_bottom;
+    //creating gameobjects to represent the objects being spawned
     GameObject topob;
     GameObject bottomob;
+    //indexes being made for the arrays
     int index;
     int index2;
-
+//float for the interval between spawn
     public float spawnRate = 10.0f;
+    //when is the next spawn
     private float nextSpawn = 0.0f;
 
     
@@ -55,16 +58,17 @@ public class SpawnObstacles : MonoBehaviour
         
             
             
-            
+            //if time is greater than the time for the next spawn run the spawner
                 if(Time.time > nextSpawn)
             {
-
+              //indexes being set to a random object within the array
                 index = Random.Range (0, obstacle_top.Length);
                 topob = obstacle_top[index];
 
                 index2 = Random.Range (0, obstacle_bottom.Length);
                 bottomob = obstacle_bottom[index];
                 
+                //instantiating the objects
                 Instantiate(topob, transform.position + new Vector3(29, 2, 0), transform.rotation);
                 Instantiate(bottomob, transform.position + new Vector3(29, -2, 0), transform.rotation);
                 //scoreTotal += scoreIncreasePerSecond * Time.deltaTime;
--- Yaris Driver/Assets/Scripts/scoreCounter.cs
+++ Yaris Driver/Assets/Scripts/scoreCounter.cs
@@ -42,23 +42,27 @@ public class scoreCounter : MonoBehaviour
         scoreTotal += scoreIncreasePerSecond * Time.deltaTime;
        
 
-       //if statements used to change the difficulty of the game in regards to the score
+       //if the score is above 20 spawner changes to spawner 2
         if (scoreTotal > 20.0f){
             spawner1.SetActive(false);
             spawner2.SetActive(true);
         }
+        //if the score is above 60 spawner changes to spawner 3
          if (scoreTotal > 60.0f){
             spawner2.SetActive(false);
             spawner3.SetActive(true);
         }
+        //if the score is above 100 spawner changes to spawner 4
           if (scoreTotal > 100.0f){
             spawner3.SetActive(false);
             spawner4.SetActive(true);
         }
+        //if the score is above 140 spawner changes to spawner 5
          if (scoreTotal > 140.0f){
             spawner4.SetActive(false);
             spawner5.SetActive(true);     
          }
+         //if the score is above 180 spawner changes to spawner 6
          if (scoreTotal > 180.0f){
             spawner5.SetActive(false);
             spawner6.SetActive(true);
